f91d9e97d39eb115f1f766d7dbcb7989c4c6ac26
Yurii <iyureg@gmail.com>
2023-02-14 22:18:36 +0300

    найти максимальную цифру в трехзначном числе

---
+++ sem2/MaxItemOfThree/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа выводит случаяное трехзначное число
+// и удаляет вторую цифру этого числа
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num = new Random().Next(100, 1000);
+int hundreds = num / 100;
+int decades = (num - (hundreds * 100)) / 10;
+int remains = num % 10;
+
+int result = hundreds * 10 + remains;
+
+Console.WriteLine($"{num} - {hundreds} - {decades} - {remains} - {result}");
+Console.WriteLine($"Результат: {result}");
