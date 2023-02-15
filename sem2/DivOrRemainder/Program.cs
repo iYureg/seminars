0fa10d588d79602b8e8606f13f5dfa8afbf5e52b
Yurii <iyureg@gmail.com>
2023-02-15 12:02:02 +0300

    кратно или остаток от деления

---
+++ sem2/DivOrRemainder/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа принимает на вход два числа и выводит, является ли второе
+// число кратным первому.
+// Если второе число не кратно первому, то выводим остаток от деления.
+
+int dividend = new Random().Next(10, 100);
+int divider = new Random().Next(1, 10);
+
+if( dividend % divider == 0)
+{
+    Console.WriteLine($"{dividend} / {divider} = {dividend / divider}");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {dividend} на {divider}: {dividend % divider}");
+}
