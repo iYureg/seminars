452df555faea592c8a2a3eb5dfcdfe48bfd40b26
Yurii <iyureg@gmail.com>
2023-02-21 19:59:18 +0300

    колличество цифр в числе

---
+++ sem4/GetDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿// программа принимает на вход число
+// и выбодит количество цифр в этом числе
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int GetDigit(int num){
+    int result = 0;
+    
+    if(num < 0) num *= -1;
+
+    while(num > 0){
+        result += 1;
+        num /= 10;
+
+    }
+    return result;
+}
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write($"В числе {N}  {GetDigit(N)} цифр");
