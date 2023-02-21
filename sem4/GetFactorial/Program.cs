3670259cfa3f8d8e60d5d51a1ee519f12972fefe
Yurii <iyureg@gmail.com>
2023-02-21 20:34:18 +0300

    факториал чисел от N

---
+++ sem4/GetFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿// принимаем на вход число N
+// и выводит произведение всех сислел от 1 до N
+
+int GetFactorial(int num)
+{
+    int result = 1;
+
+    int i = 2;
+
+    if (num < 0)
+    {
+        i = num;
+        num = - 1;
+    }
+
+    for (; i <= num; i++)
+    {
+        result *= i;
+
+    }
+        return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Произведение чисел от {N} -> {GetFactorial(N)}");
+
