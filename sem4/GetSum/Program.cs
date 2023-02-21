923c8c261adbf7a1691f58dd3feaf258b9d75799
Yurii <iyureg@gmail.com>
2023-02-21 17:41:49 +0300

    метод возвращает сумму чисел от 1 до N

---
+++ sem4/GetSum/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа принимает на вход число А
+// и выдает сумму чисел от 1 до А
+
+int GetSum(int n){
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число A: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма чисел от 1 до {num} = {GetSum(num)}");
