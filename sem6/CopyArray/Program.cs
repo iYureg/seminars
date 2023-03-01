750de69a6abfd775da5b19f6e52c4b599a47366a
Yurii <iyureg@gmail.com>
2023-03-01 16:01:04 +0300

    программа делает копию массива

---
+++ sem6/CopyArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// написать программу, которая делает копию массива
+// с помощью поэлементного копирования
+int[] GetRandomArray(int size){
+
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+
+    return array;
+}
+
+int[] GetArrayCopy(int[] arr){
+    int[] copy = new int[arr.Length];
+
+    for (int i = 0; i < copy.Length; i++)
+    {
+        copy[i] = arr[i];
+    }
+
+    return copy;
+}
+
+int[] randomArr = GetRandomArray(10);
+int[] copyArr = GetArrayCopy(randomArr);
+Console.WriteLine($"[ {String.Join(", ", randomArr)} ] copy -> [ {String.Join(", ", copyArr)} ]");
