e90fa5e0b9096d061503236f5ab52cc9c3359d50
Yurii <iyureg@gmail.com>
2023-03-01 15:19:49 +0300

    программа переводит Dec -> Bin/Oct/Hex

---
+++ sem6/DecToBin/Program.cs
@@ -0,0 +1,27 @@
+﻿// программа преобразовывает десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string GetBin(int dec, int otherSystem)
+{    
+    string res = String.Empty;
+    string nums = "0123456789ABCDEF";
+
+    while (dec > 0)
+    {
+        res = nums[dec%otherSystem] + res;
+        dec /= otherSystem;
+    }
+    return res;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"[ {GetBin(N, 2)} ]");
+
+
+
+
