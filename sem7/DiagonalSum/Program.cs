bfc06e66b0962ae9cdced153ce7f0a2db8680ae2
Yurii <iyureg@gmail.com>
2023-03-04 12:10:38 +0300

    сумма элементов двумерного массива по главной диагонали

---
+++ sem7/DiagonalSum/Program.cs
@@ -0,0 +1,55 @@
+﻿// 
+
+using System;
+using static System.Console;
+
+int[,] GetRandomTwodimArray(int size1, int size2){
+
+    int[,] array = new int[size1,size2];
+
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] inArray){
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]}");
+            Write(" ");
+        }
+        WriteLine();
+    }
+}
+
+int GetDiagSum(int[,] arr){
+    int length = arr.GetLength(0) < arr.GetLength(1) ? arr.GetLength(0) : arr.GetLength(1);
+    int result = 0;
+    for (int i = 0; i < length; i++)
+    {
+        result += arr[i, i];    
+    }
+    return result;
+}
+
+Clear();
+Write("Введите количество строк в массиве: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(ReadLine());
+
+int[,] arr = GetRandomTwodimArray(rows, columns);
+PrintArray(arr);
+WriteLine();
+
+WriteLine($"Сумма элементов по диагонали -> {GetDiagSum(arr)}");
+
